4cee6acb8cc6c81f86848f5caad91fecfd2d4e86
MSekrst <matija.sekrst13@gmail.com>
2021-03-23 14:45:34 +0100

    Lesson 4 material

---
+++ lessons/04-files/List.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const defaultState = ['Hello', 'Again', 'SofaScore', 'Academy']
+
+export class List extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { words: defaultState }
+  }
+
+  render() {
+    const { words } = this.state
+
+    return (
+      <div>
+        <h3>
+          {words.map((word, index) => (
+            <span key={`${word}-${index}`} style={{ marginLeft: 8 }}>
+              {word}
+            </span>
+          ))}
+        </h3>
+
+        <button
+          onClick={() => {
+            const wordsCopy = [...words]
+            this.setState({ words: wordsCopy.sort() })
+          }}
+        >
+          SORT
+        </button>
+        <button
+          style={{ marginLeft: 4 }}
+          onClick={() => {
+            this.setState({ words: defaultState })
+          }}
+        >
+          REVERT
+        </button>
+      </div>
+    )
+  }
+}
